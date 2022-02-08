b369832cddb1802b93e3046505d039187d4332f9
mustaphash <mshakir9918@gmail.com>
2022-02-08 23:59:11 +0200

    Add Controllers

---
+++ src/Pharmacy/Controllers/ClientController.cs
@@ -0,0 +1,27 @@
+﻿using Core.Entities;
+using Core.Queries;
+using DAL.Queries.GetAllClients;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Pharmacy.Controllers
+{
+    [Route("clients")]
+    [ApiController]
+    public class ClientController : ControllerBase
+    {
+        private readonly IQueryHandler<GetAllClientsQuery, IList<Client>> _getAllClientsQuery;
+        public ClientController(
+            IQueryHandler<GetAllClientsQuery, IList<Client>> getAllClientsQuery)
+        {
+            _getAllClientsQuery = getAllClientsQuery;
+        }
+
+        [HttpGet(Name = "GetAllClients")]
+        public async Task<IActionResult> GetAllClients()
+        {
+            IList<Client> clients = await _getAllClientsQuery.HandleAsync(new GetAllClientsQuery());
+
+            return Ok(clients);
+        }
+    }
+}
---
+++ src/Pharmacy/Controllers/PharmacyController.cs
@@ -0,0 +1,14 @@
+﻿using Core.Queries;
+using DAL.Queries.GetAllPharmacies;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Pharmacy.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PharmacyController : ControllerBase
+    {
+        private readonly IQueryHandler<GetAllPharmaciesQuery, IList<Pharmacy>>
+    }
+}

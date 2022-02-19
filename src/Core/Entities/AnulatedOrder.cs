daa10ad62fc31f0e91ec5ac0a29a8b0b3fcdfbf7
Mustafa Shakir <mshakir9918@gmail.com>
2022-02-19 19:36:01 +0200

    Add AnulatedOrder Class

---
+++ src/Core/Entities/AnulatedOrder.cs
@@ -0,0 +1,15 @@
+﻿namespace Core.Entities
+{
+    public class AnulatedOrder
+    {
+        public int Id { get; set; }
+        public int ClientId { get; set; }
+        public int MedicamentId { get; set; }
+        public int PharmacyId { get; set; }
+        public string Status { get; set; }
+
+        public Client Client { get; set; }
+        public Medicament Medicament { get; set; }
+        public Pharmacy Pharmacy { get; set; }
+    }
+}

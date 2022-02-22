5b615be1ee88f86e251eb8299eb4d6f532bb5fe9
Mustafa Shakir <mshakir9918@gmail.com>
2022-02-22 22:11:44 +0200

    Create PharmacyModel

---
+++ src/Pharmacy/Models/CreatePharmacyModel.cs
@@ -0,0 +1,19 @@
+﻿namespace Pharmacy.Models
+{
+    public class CreatePharmacyModel
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int PhoneNumber { get; set; }
+
+        public Core.Entities.Pharmacy ToPharmacy()
+        {
+            return new Core.Entities.Pharmacy()
+            {
+                Name = this.Name,
+                Address = this.Address,
+                PhoneNumber = this.PhoneNumber,
+            };
+        }
+    }
+}

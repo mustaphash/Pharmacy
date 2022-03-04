2acd58fb2fea5884aeb8b5f7b65f92caf8b0b43f
Mustafa Shakir <mshakir9918@gmail.com>
2022-03-04 20:30:50 +0200

    Create BuyOrAnulateOrderModel

--- src/Core/Entities/Order.cs
+++ src/Core/Entities/Order.cs
@@ -4,9 +4,11 @@
     {
         public int Id { get; set; }
         public int ClientId { get; set; }
-        public int PharmacyMedicamentId { get; set; }
+        public int PharmacyId { get; set; }
+        public int MedicamentId { get; set; }
 
+        public Pharmacy Pharmacy { get; set; }
+        public Medicament Medicament { get; set; }
         public Client Client { get; set; }
-        public PharmacyMedicament PharmacyMedicament { get; set; }
     }
 }
---
+++ src/Pharmacy/Models/OrderModel/BuyOrAnulateMedicationModel.cs
@@ -0,0 +1,33 @@
+﻿using Core.Entities;
+
+namespace Pharmacy.Models.OrderModel
+{
+    public class BuyOrAnulateMedicationModel
+    {
+        public int MedicationId { get; set; }
+
+        public int PharmacyId { get; set; }
+
+        public int ClientId { get; set; }
+
+        public Order ToOrder()
+        {
+            return new Order()
+            {
+                ClientId = ClientId,
+                MedicamentId = MedicationId,
+                PharmacyId = PharmacyId,
+            };
+        }
+
+        public AnulatedOrder ToAnulatedOrder()
+        {
+            return new AnulatedOrder()
+            {
+                ClientId = ClientId,
+                MedicamentId = MedicationId,
+                PharmacyId = PharmacyId,
+            };
+        }
+    }
+}

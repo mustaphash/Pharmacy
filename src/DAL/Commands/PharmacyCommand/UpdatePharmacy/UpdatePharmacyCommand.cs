bd2f78dd757d12d109c1e6e724357978f80c34ed
Mustafa Shakir <mshakir9918@gmail.com>
2022-03-08 21:53:14 +0200

    Add UpdatePharmacy Command and CommandHandler

---
+++ src/DAL/Commands/PharmacyCommand/UpdatePharmacy/UpdatePharmacyCommand.cs
@@ -0,0 +1,14 @@
+﻿using Core.Commands;
+using Core.Entities;
+
+namespace DAL.Commands.PharmacyCommand.UpdatePharmacy
+{
+    public class UpdatePharmacyCommand : ICommand
+    {
+        public UpdatePharmacyCommand(Pharmacy pharmacy)
+        {
+            Pharmacy = pharmacy;
+        }
+        public Pharmacy Pharmacy { get; set; }
+    }
+}
---
+++ src/DAL/Commands/PharmacyCommand/UpdatePharmacy/UpdatePharmacyCommandHandler.cs
@@ -0,0 +1,21 @@
+﻿using Core.Commands;
+
+namespace DAL.Commands.PharmacyCommand.UpdatePharmacy
+{
+    public class UpdatePharmacyCommandHandler : ICommandHandler<UpdatePharmacyCommand>
+    {
+        private readonly PharmacyContext _pharmacyContext;
+        public UpdatePharmacyCommandHandler(PharmacyContext pharmacyContext)
+        {
+            _pharmacyContext = pharmacyContext;
+        }
+        public async Task HandleAsync(UpdatePharmacyCommand command, CancellationToken cancellationToken = default)
+        {
+            if (command != null)
+            {
+                _pharmacyContext.Update(command.Pharmacy);
+                await _pharmacyContext.SaveChangesAsync();
+            }
+        }
+    }
+}

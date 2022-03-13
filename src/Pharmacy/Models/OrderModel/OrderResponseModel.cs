45427aa5838df68ca4b3405794108c94492cd609
Mustafa Shakir <mshakir9918@gmail.com>
2022-03-13 10:33:51 +0200

    Create Report Models

---
+++ src/Pharmacy/Models/OrderModel/OrderResponseModel.cs
@@ -0,0 +1,28 @@
+﻿using Core.Entities;
+
+namespace Pharmacy.Models.OrderModel
+{
+    public class OrderResponseModel
+    {
+        public OrderResponseModel(Order order)
+        {
+            Id = order.Id;
+            ClientName = order.Client.FirstName;
+            PharmacyName = order.Pharmacy.Name;
+            MedicamentName = order.Medicament.Name;
+            CreateDate = order.CreateDate;
+        }
+
+        public int Id { get; set; }
+
+        // ClientName
+        public string ClientName { get; set; }
+
+        // Pharmacy Name
+        public string PharmacyName { get; set; }
+
+        // MedicamentName
+        public string MedicamentName { get; set; }
+        public DateTime CreateDate { get; set; }
+    }
+}
---
+++ src/Pharmacy/Models/ReportModel/ReportResponseModel.cs
@@ -0,0 +1,32 @@
+﻿using Core.Entities;
+using Pharmacy.Models.OrderModel;
+
+namespace Pharmacy.Models.ReportMode
+{
+    public class ReportResponseModel
+    {
+        public ReportResponseModel(Report report)
+        {
+            TotalRevenue = report.TotalRevenue;
+            StartDate = report.StartDate;
+            EndDate = report.EndDate;
+            Orders = report?.Orders?.Select(o => new OrderResponseModel(o));
+        }
+
+        public decimal TotalRevenue { get; set; }
+
+        public DateTime? StartDate { get; set; }
+
+        public DateTime? EndDate { get; set; }
+
+        // TODO: Fix for anullated orders
+        // public int AnullatedOrdersCount => AnulatedOrders != null ? AnulatedOrders.Count() : 0;
+
+        public int OrdersCount => Orders != null ? Orders.Count() : 0;
+
+        // TODO: Fix for anullated orders
+        //public IEnumerable<AnulatedOrder> AnulatedOrders { get; set; }
+
+        public IEnumerable<OrderResponseModel> Orders { get; set; }
+    }
+}

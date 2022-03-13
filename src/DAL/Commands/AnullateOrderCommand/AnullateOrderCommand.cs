aace0e45e6ffadef4fccd6ddea8fe2053067a4b1
Mustafa Shakir <mshakir9918@gmail.com>
2022-03-13 10:18:04 +0200

    Add Command for AnullateOrder

---
+++ src/DAL/Commands/AnullateOrderCommand/AnullateOrderCommand.cs
@@ -0,0 +1,15 @@
+﻿using Core.Commands;
+using Core.Entities;
+
+namespace DAL.Commands.AnullateOrderCommand
+{
+    public class AnullateOrderCommand : ICommand
+    {
+        public AnullateOrderCommand(AnullatedOrder order)
+        {
+            Order = order;
+        }
+
+        public AnullatedOrder Order { get; }
+    }
+}
---
+++ src/DAL/Commands/AnullateOrderCommand/AnullateOrderCommandHandler.cs
@@ -0,0 +1,18 @@
+﻿using Core.Commands;
+
+namespace DAL.Commands.AnullateOrderCommand
+{
+    public class AnullateOrderCommandHandler : ICommandHandler<AnullateOrderCommand>
+    {
+        private readonly PharmacyContext _pharmacyContext;
+        public AnullateOrderCommandHandler(PharmacyContext pharmacyContext)
+        {
+            _pharmacyContext = pharmacyContext;
+        }
+        public async Task HandleAsync(AnullateOrderCommand command, CancellationToken cancellationToken = default)
+        {
+            await _pharmacyContext.AnullatedOrders.AddAsync(command.Order);
+            await _pharmacyContext.SaveChangesAsync();
+        }
+    }
+}

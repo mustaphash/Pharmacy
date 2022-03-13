6bc3118e05827ca440657796cf2fe1fbfc3a4660
Mustafa Shakir <mshakir9918@gmail.com>
2022-03-13 10:35:57 +0200

    Fix OrderController

--- src/Pharmacy/Controllers/OrderController.cs
+++ src/Pharmacy/Controllers/OrderController.cs
@@ -1,8 +1,13 @@
 ﻿using Core.Commands;
+using Core.Entities;
+using Core.Queries;
+using DAL.Commands.AnullateOrderCommand;
 using DAL.Commands.OrderCommand;
+using DAL.Queries.Report;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Pharmacy.Models.OrderModel;
+using Pharmacy.Models.ReportMode;
 
 namespace Pharmacy.Controllers
 {
@@ -15,10 +20,18 @@ namespace Pharmacy.Controllers
         //Anulirane na poruchka(AnulatedOrders s kolona Status)
         //Otchet za anulirani poruchki za period
         private readonly ICommandHandler<OrderCommand> _createOrderCommand;
+        private readonly ICommandHandler<AnullateOrderCommand> _anullateOrderCommand;
+        private readonly IQueryHandler<GetReportQuery, Report> _getReport;
+
+
         public OrderController(
-            ICommandHandler<OrderCommand> createOrderCommand)
+            ICommandHandler<OrderCommand> createOrderCommand,
+            ICommandHandler<AnullateOrderCommand> anullateOrderCommand,
+            IQueryHandler<GetReportQuery, Report> getReport)
         {
             _createOrderCommand = createOrderCommand;
+            _anullateOrderCommand = anullateOrderCommand;
+            _getReport = getReport;
         }
 
         [HttpPost("buy")]
@@ -33,8 +46,8 @@ namespace Pharmacy.Controllers
         [HttpPost("anullate")]
         public async Task<IActionResult> AnulateOrder(BuyOrAnulateMedicationModel model)
         {
-            var order = model.ToOrder();
-            await _createOrderCommand.HandleAsync(new OrderCommand(order));
+            var order = model.ToAnulatedOrder();
+            await _anullateOrderCommand.HandleAsync(new AnullateOrderCommand(order));
 
             return NoContent();
         }
@@ -43,8 +56,10 @@ namespace Pharmacy.Controllers
         [HttpGet("report")]
         public async Task<IActionResult> Report(ReportModel model)
         {
+            Report report = await _getReport.HandleAsync(new GetReportQuery(model.StartDate, model.EndDate));
+            var reportResponse = new ReportResponseModel(report);
 
-            return NoContent();
+            return Ok(reportResponse);
         }
     }
 }

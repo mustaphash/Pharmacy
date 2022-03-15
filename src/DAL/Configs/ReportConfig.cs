1aff8b8c104028385ca305106ba13b186ab65644
mustaphash <mshakir9918@gmail.com>
2022-03-15 19:22:42 +0200

    Create ReportConfig

---
+++ src/DAL/Configs/ReportConfig.cs
@@ -0,0 +1,17 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DAL.Configs
+{
+    public class ReportConfig : IEntityTypeConfiguration<Report>
+    {
+        public void Configure(EntityTypeBuilder<Report> builder)
+        {
+            builder.HasKey(r => r.TotalRevenue);
+
+            builder.Property(r => r.StartDate);
+            builder.Property(r => r.EndDate);
+        }
+    }
+}

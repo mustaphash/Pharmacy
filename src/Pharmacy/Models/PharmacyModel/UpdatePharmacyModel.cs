724e13fdf0aba5cc274123112721737f381f8e18
mustaphash <mshakir9918@gmail.com>
2022-03-22 18:59:23 +0200

    Create UpdatePharmacyModel

---
+++ src/Pharmacy/Models/PharmacyModel/UpdatePharmacyModel.cs
@@ -0,0 +1,24 @@
+﻿namespace Pharmacy.Models.PharmacyModel
+{
+    public class UpdatePharmacyModel
+    {
+        public UpdatePharmacyModel()
+        {
+            Name = String.Empty;
+            Address = String.Empty;
+        }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int PhoneNumber { get; set; }
+
+        public Core.Entities.Pharmacy UpdatePharmacy()
+        {
+            return new Core.Entities.Pharmacy()
+            {
+                Name = Name,
+                Address = Address,
+                PhoneNumber = PhoneNumber,
+            };
+        }
+    }
+}
